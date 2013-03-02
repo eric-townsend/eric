4c1d356a26f9acab92bcccc4233216108e9a8e5f
Eric <Eric@Eric.local>
2013-03-01 18:14:05 -0800

    first

---
+++ js/plugins.js
@@ -0,0 +1,34 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}()); 
+// Preload 
+$(document).ready(function () {
+		$("#wrap").queryLoader2({
+		barColor: "#fff",
+		backgroundColor: "#000",
+		percentage: true,
+		barHeight: 1,
+		completeAnimation: "fade",
+		minimumTime: 3000
+	});
+
+});
